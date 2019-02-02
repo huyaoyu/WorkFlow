3e4d6ea61d27fb06158818c4ed511f5711405e72
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2019-02-02 11:54:23 -0500

    Add a naive setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='WorkFlow',
+      version='1.0',
+      description='Save effort for logging and visualization in the work flow of training deep nerual network.',
+      author='Yaoyu Hu, Wenshan Wang',
+      author_email='yyhu_live@outlook.com',
+      url='huyaoyu.com',
+      packages=['workflow']
+     )
