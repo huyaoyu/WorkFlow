8f10ffe329664e5f9aeb00d6fa6a58bf33c3f04f
yyhu <huyaoyu1986@gmail.com>
2019-05-05 01:38:17 -0400

    Add semi-log to append_plotter() of TorchFlow.

--- workflow/TorchFlow.py
+++ workflow/TorchFlow.py
@@ -59,9 +59,9 @@ class TorchFlow(WorkFlow.WorkFlow):
         modelname = self.prefix + modelname + self.suffix + '.pkl'
         torch.save(model.state_dict(), os.path.join(self.modeldir, modelname))
 
-    def append_plotter(self, plotName, valueNameList, smoothList):
+    def append_plotter(self, plotName, valueNameList, smoothList, semiLog=False):
         if self.plotterType == 'Visdom':
-            self.AVP.append(WorkFlow.VisdomLinePlotter(plotName, self.AV, valueNameList, smoothList))
+            self.AVP.append(WorkFlow.VisdomLinePlotter(plotName, self.AV, valueNameList, smoothList, semiLog=semiLog))
         elif self.plotterType == 'Int':
-            self.AVP.append(WorkFlow.PLTIntermittentPlotter(self.workingDir + "/IntPlot", plotName, self.AV, valueNameList, smoothList))
+            self.AVP.append(WorkFlow.PLTIntermittentPlotter(self.workingDir + "/IntPlot", plotName, self.AV, valueNameList, smoothList, semiLog=semiLog))
 
